715bda9092b0eed0ed6db13639e1b7287d6e8944
ctwc55 <ssy990408@gmail.com>
2022-11-03 04:10:09 +0900

    init project

---
+++ src/main/java/dhg/aieduplatform/AiEduPlatformApplication.java
@@ -0,0 +1,13 @@
+package dhg.aieduplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEduPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEduPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhg/aieduplatform/AiEduPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dhg.aieduplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEduPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
